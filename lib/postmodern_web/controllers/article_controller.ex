2614b383b905d3bac493441ae87ffa2ec57af2c8
Sean Tilley <sean@deadsuperhero.com>
2018-06-09 02:15:35 -0700

    use a plug to prevent non-authed users from doing anything with articles

--- lib/postmodern_web/controllers/article_controller.ex
+++ lib/postmodern_web/controllers/article_controller.ex
@@ -3,6 +3,23 @@ defmodule PostmodernWeb.ArticleController do
 
   alias Postmodern.Stories
   alias Postmodern.Stories.Article
+  alias Postmodern.Accounts
+
+  plug :check_auth when action in [:new, :create, :edit, :update, :delete]
+
+  defp check_auth(conn, _args) do
+    if user_id = get_session(conn, :current_user_id) do
+      current_user = Accounts.get_user!(user_id)
+
+      conn
+      |> assign(:current_user, current_user)
+    else
+      conn
+      |> put_flash(:error, "You need to be signed in to access that page.")
+      |> redirect(to: article_path(conn, :index))
+      |> halt()
+    end
+  end
 
   def index(conn, _params) do
     articles = Stories.list_articles()
