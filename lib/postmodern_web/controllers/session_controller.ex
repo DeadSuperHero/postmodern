cbc61471e80a1421f57404f324efe490ad7ccedc
Sean Tilley <sean@deadsuperhero.com>
2018-06-09 02:40:05 -0700

    attempts to log in without a real account now fails

--- lib/postmodern_web/controllers/session_controller.ex
+++ lib/postmodern_web/controllers/session_controller.ex
@@ -10,14 +10,13 @@ defmodule PostmodernWeb.SessionController do
 
   def create(conn, %{"session" => auth_params}) do
     user = Accounts.get_by_username(auth_params["username"])
-
     case Comeonin.Bcrypt.check_pass(user, auth_params["password"])  do
       {:ok, user} ->
         conn
         |> put_session(:current_user_id, user.id)
         |> put_flash(:info, "Signed in successfully")
         |> redirect(to: article_path(conn, :index))
-      {:error, } ->
+      {:error, _} ->
         conn
         |> put_flash(:error, "There was a problem with your username/password")
         |> render("new.html")
